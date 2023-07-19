c0a1179c80dc49791950e13ebe5835eb808c271c
Catu Hardoy <catuhardoy@MacBook-Air-de-Catu.local>
2023-07-19 18:59:39 -0300

    newconection

--- src/app/libs/mongodb.js
+++ src/app/libs/mongodb.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGODB_URI);
+        // await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect('mongodb+srv://catalinahardoy:admin@cluster0.pohmvyb.mongodb.net/todo?retryWrites=true&w=majority');
+
         console.log('connected')
     }
     catch(error){
